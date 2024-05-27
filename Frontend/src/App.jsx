c36a646dbc07ad138e571681193add29e4eebea0
YourGitHubUsername <YourGitHubEmail>
2024-05-27 15:58:05 +0530

    added something

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
 }
 
-export default App
+export default App;
